7721d0cb42a7eb522131184b6c5d8be0fad4c8e3
Gabor Makeev <gabor.makejev@gmail.com>
2023-06-05 14:44:27 +0200

    feat: create user welcome page, about page and news page (task 3 completed)

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/welcome/{user}', function (string $user) {
+    return "Welcome $user";
+});
+
+Route::get('/about', function () {
+    return 'About';
+});
+
+Route::get('/news', function () {
+    return 'News';
+});
