6be38aae438863bf952859369eb2de0d77b532de
Gabor Makeev <gabor.makejev@gmail.com>
2023-06-19 22:36:29 +0200

    feat: task 1.b. completed; create data request controller, create data request form, add data request create and store routes, add data request page link to menu

--- resources/views/components/menu.blade.php
+++ resources/views/components/menu.blade.php
@@ -4,5 +4,6 @@
         <a class="nav-item nav-link link-body-emphasis" href="/categories">Categories</a>
         <a class="nav-item nav-link link-body-emphasis" href="/about">About</a>
         <a class="nav-item nav-link link-body-emphasis" href="/create">Create article</a>
+        <a class="nav-item nav-link link-body-emphasis" href="/request/create">Request data</a>
     </nav>
 </div>
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AboutPageController;
 use App\Http\Controllers\CategoriesController;
 use App\Http\Controllers\CreateArticlePageController;
+use App\Http\Controllers\DataRequestController;
 use App\Http\Controllers\NewsController;
 use App\Http\Controllers\SignInPageController;
 use Illuminate\Support\Facades\Route;
@@ -33,3 +34,7 @@ Route::get('/create', [CreateArticlePageController::class, 'index']);
 Route::get('/signin', [SignInPageController::class, 'index']);
 
 Route::get('/about', [AboutPageController::class, 'index']);
+
+Route::get('/request/create', [DataRequestController::class, 'create']);
+
+Route::post('/request/store', [DataRequestController::class, 'store']);
