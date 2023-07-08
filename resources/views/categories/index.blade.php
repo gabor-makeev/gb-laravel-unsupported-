64f8855762dbc2177a16bf61240e3bb7743ad49e
Gabor Makeev <gabor.makejev@gmail.com>
2023-07-09 00:39:21 +0200

    feat: complete task 4; implement category and news data getting from the database

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -4,7 +4,7 @@
 <h1>Categories</h1>
 <div class="list-group">
     @foreach($categories as $category)
-        <a href="/categories/{{ $category }}" class="list-group-item list-group-item-action">{{ ucfirst($category) }}</a>
+        <a href="/categories/{{ $category->id }}" class="list-group-item list-group-item-action">{{ ucfirst($category->name) }}</a>
     @endforeach
 </div>
 @endsection
--- resources/views/news/show.blade.php
+++ resources/views/news/show.blade.php
@@ -1,10 +1,10 @@
 @extends('layouts.main')
-@section('title')| {{ $article['title'] }} @stop
+@section('title')| {{ $article->title }} @stop
 @section('content')
 <article class="blog-post">
-    <h2 class="display-5 link-body-emphasis mb-1">{{ $article['title'] }}</h2>
-    <p>{{ $article['short_description'] }}</p>
+    <h2 class="display-5 link-body-emphasis mb-1">{{ $article->title }}</h2>
+    <p>{{ $article->preview_content }}</p>
     <hr>
-    <p>{{ $article['description'] }}</p>
+    <p>{{ $article->content }}</p>
 </article>
 @endsection
